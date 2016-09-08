efe99bdd2982bcab874d6dd7d6a2a6e58f22fd47
Diego Legua <Diego@macbook-pro-skullone.local>
2016-09-08 11:22:57 +0200

    Implemented basic search interface

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Diego Legua on 8/09/16.
+//  Copyright © 2016 DL.Training. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
